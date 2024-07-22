5ae7da9f00ded08dac36178b00164bb5c15badd8
Anmol <anmol36pandey@gmail.com>
2024-07-22 19:15:25 +0530

    change cors link

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 //app.use(cors());
 app.use(
   cors({
-   origin:"http://localhost:3000",
+   origin:"https://study-notion-anmol-pandey--iota.vercel.app/",
     credentials: true,
     maxAge: 14400,
   })
