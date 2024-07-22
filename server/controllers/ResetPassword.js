fa552d6d4f9c3570410e696e9db55478d534dbcb
Anmol <anmol36pandey@gmail.com>
2024-07-22 19:58:21 +0530

    final commit

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -25,7 +25,7 @@ exports.resetPasswordToken = async (req, res) => {
 		);
 		console.log("DETAILS", updatedDetails);
 
-		const url = `https://localhost:3000/update-password/${token}`;
+		const url = `https://study-notion-anmol-pandey--iota.vercel.app/update-password/${token}`;
 
 		await mailSender(
 			email,
